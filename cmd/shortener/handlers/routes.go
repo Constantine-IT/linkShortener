b3d03f0e7b35be8f280d84fccc0e33299ab97069
Constantine_IT <33015794+Constantine-IT@users.noreply.github.com>
2022-04-03 08:59:04 +0700

    add compression support

--- cmd/shortener/handlers/routes.go
+++ cmd/shortener/handlers/routes.go
@@ -12,6 +12,7 @@ func Routes() chi.Router {
 
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
 	r.Use(middleware.RequestID)
+	r.Use(middleware.Compress(5))
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
