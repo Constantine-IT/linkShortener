3a6d3f4b163bb30322bcdf293b8a3126b2a098cc
Constantine_IT <33015794+Constantine-IT@users.noreply.github.com>
2022-04-03 16:44:17 +0700

    change order of middleware

--- cmd/shortener/handlers/routes.go
+++ cmd/shortener/handlers/routes.go
@@ -11,9 +11,9 @@ func Routes() chi.Router {
 	r := chi.NewRouter()
 
 	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	r.Use(middleware.Compress(1, "text/plain", "application/json"))
+	r.Use(middleware.AllowContentEncoding("gzip"))
 	r.Use(middleware.RequestID)
-	r.Use(middleware.Compress(1, `text/plain`, `application/json`))
-	r.Use(middleware.AllowContentEncoding(`gzip`))
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
