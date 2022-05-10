868de63bd42f1f223717996fd41f2f036c16157f
Constantine_IT <33015794+Constantine-IT@users.noreply.github.com>
2022-05-10 14:21:42 +0700

    readme correction

--- cmd/shortener/internal/handlers/routes.go
+++ cmd/shortener/internal/handlers/routes.go
@@ -34,13 +34,7 @@ func (app *Application) Routes() chi.Router {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	//	Эндпоинт GET /api/user/urls считывает UserID из cookie запроса и выдаёт все URL, сохраненные этим пользователем.
-	//	Эндпоинт GET /{id} принимает в качестве URL-параметра идентификатор сокращённого URL и возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
-	//	Эндпоинт GET /ping проверяет доступность базы данных, выдает ответ с кодом 200, если доступна, и 500 - если не доступна.
-	//	Эндпоинт POST / принимает в теле запроса в виде текста строку URL для сокращения и возвращает ответ с кодом 201 и сокращённым URL в виде текстовой строки в теле.
-	//	Эндпоинт POST /api/shorten - аналогичен предыдущему, но принимает в теле запроса JSON-объект {"url":"<some_url>"} и возвращает в теле ответа JSON-объект {"result":"<shorten_url>"}.
-	//	Эндпоинт POST /api/shorten/batch, принимает в теле запроса множество URL для сокращения в формате JSON и возвращает сокращенные URL в JSON формате
-	//	Эндпоинт DELETE /api/user/urls, принимает задания на удаление списка ранее сформированных URL
+	//	маршруты сервера и их обработчики
 	r.Route("/", func(r chi.Router) {
 		r.Delete("/api/user/urls", app.DeleteURLByUserIDHandler)
 		r.Get("/api/user/urls", app.GetURLByUserIDHandler)
